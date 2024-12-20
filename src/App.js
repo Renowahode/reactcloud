cacf1c2faf255ecc658890bd969ba9d0e9e4ceac
Wence24 <wencedantedeveraddc@gmail.com>
2024-12-20 23:07:05 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App!</h1>
+      <p>This is a simple React application deployed using Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
